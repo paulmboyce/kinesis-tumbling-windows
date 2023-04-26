0579f43dd4d1a8428d878c5adc0f67597ff8834c
EC2 Default User <paulmboyce@gmail.com>
2023-04-26 16:05:46 +0000

    feature(add files): licence and package lock

---
+++ lambda/index.js
@@ -0,0 +1,59 @@
+const AWS = require("aws-sdk");
+AWS.config.update({ region: process.env.AWS_REGION });
+const docClient = new AWS.DynamoDB.DocumentClient();
+const TableName = process.env.TABLE_NAME;
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+exports.handler = async (event) => {
+  // Save aggregation result in the final invocation
+  if (event.isFinalInvokeForWindow) {
+    console.log("Final: ", event);
+
+    const params = {
+      TableName,
+      Item: {
+        windowStart: event.window.start,
+        windowEnd: event.window.end,
+        distance: Math.round(event.state.distance),
+        shardId: event.shardId,
+        name: event.state.name,
+      },
+    };
+    console.log({ params });
+    await docClient.put(params).promise();
+  }
+  console.log(JSON.stringify(event, null, 2));
+
+  // Create the state object on first invocation or use state passed in
+  let state = event.state;
+
+  if (isEmpty(state)) {
+    state = {
+      distance: 0,
+    };
+  }
+  console.log("Existing: ", state);
+
+  // Process records with custom aggregation logic
+  event.Records.map((record) => {
+    const payload = Buffer.from(record.kinesis.data, "base64").toString(
+      "ascii"
+    );
+    const item = JSON.parse(payload);
+
+    let value = item.Distance;
+    console.log("Adding: ", value);
+    state.distance += value;
+
+    let unicorn = item.Name;
+    console.log("Name: ", unicorn);
+    state.name = unicorn;
+  });
+
+  // Return the state for the next invocation
+  console.log("Returning state: ", state);
+  return { state: state };
+};
--- lib/kinesis-tumbling-windows-stack.ts
+++ lib/kinesis-tumbling-windows-stack.ts
@@ -1,16 +1,101 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ddb from "aws-cdk-lib/aws-dynamodb";
+import * as sqs from "aws-cdk-lib/aws-sqs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as kinesis from "aws-cdk-lib/aws-kinesis";
+import {
+  KinesisEventSource,
+  SqsDlq,
+} from "aws-cdk-lib/aws-lambda-event-sources";
 
 export class KinesisTumblingWindowsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const ddbUnicornAggregation = new ddb.Table(this, "UnicornAggregation", {
+      partitionKey: {
+        name: "name",
+        type: ddb.AttributeType.STRING,
+      },
+      sortKey: {
+        name: "windowStart",
+        type: ddb.AttributeType.STRING,
+      },
+      tableName: "UnicornAggregation",
+      billingMode: ddb.BillingMode.PROVISIONED,
+      readCapacity: 2,
+      writeCapacity: 2,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // ** NOT recommended for production code **
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'KinesisTumblingWindowsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambdaRole = new iam.Role(this, "WildRydesAggregatorExecutionRole", {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      description: "Role to access kinesis and dynamodb",
+      roleName: "WildRydesAggregatorExecutionRole",
+    });
+
+    lambdaRole.addManagedPolicy(
+      iam.ManagedPolicy.fromManagedPolicyArn(
+        this,
+        "Apply.AWSLambdaBasicExecutionRole",
+        "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+      )
+    );
+    // AWSLambdaKinesisExecutionRole is not neccessary!
+    //  >>> because CDK will add the actions from the AWSLambdaKinesisExecutionRole
+    // due to addEventSource
+    // but restricted to the Kinesis Stream here, which is tighter than the managaged role which
+    // gets access to ALL Kinesis Streams
+
+    lambdaRole.addToPrincipalPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        resources: [ddbUnicornAggregation.tableArn],
+        actions: ["dynamodb:PutItem"],
+      })
+    );
+    const lambdaFn = new lambda.Function(this, "Function", {
+      functionName: "WildRydesAggregator",
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: "index.handler",
+      // deadLetterQueueEnabled: true,
+      // deadLetterQueue: deadLetterQueue,
+      code: lambda.Code.fromAsset(join(__dirname, "../lambda")),
+      role: lambdaRole,
+      environment: {
+        TABLE_NAME: ddbUnicornAggregation.tableName,
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      },
+      logRetention: 3,
+    });
+
+    const stream = new kinesis.Stream(this, "wildrydes-stream", {
+      streamName: "wildrydes",
+      streamMode: kinesis.StreamMode.PROVISIONED,
+      shardCount: 1,
+    });
+    lambdaFn.addEventSource(
+      new KinesisEventSource(stream, {
+        batchSize: 100,
+        startingPosition: lambda.StartingPosition.LATEST,
+        retryAttempts: 2,
+        bisectBatchOnError: true,
+        tumblingWindow: cdk.Duration.seconds(60),
+      })
+    );
+
+    new cdk.CfnOutput(this, "DynamoDB.UnicornAggregation.ARN:", {
+      value: ddbUnicornAggregation.tableArn,
+    });
+    new cdk.CfnOutput(this, "LambdaFn.ARN", { value: lambdaFn.functionArn });
+    new cdk.CfnOutput(this, "LambdaFn.Role.ARN", {
+      value: lambdaFn.role!.roleArn,
+    });
+    new cdk.CfnOutput(this, "Kinesis.Stream.ARN", {
+      value: stream.streamArn,
+    });
   }
 }
